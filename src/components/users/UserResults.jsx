660ee1c9d9454d10d888957588fbc94e01bf06cd
Mati <mateuszlabun9@gmail.com>
2022-11-02 21:31:20 +0000

    added gothub context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+import UserItem from './UserItem'
 
 function UserResults() {
+  const {users, loading, fetchUsers} = useContext(GithubContext)
+
+  useEffect(()=>{
+    fetchUsers()
+  }, [])
+  
   return (
-    <div>UserResults</div>
+    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>{users.map((user)=>(
+        <UserItem key={user.id} user = {user} />
+    ))}</div>
   )
 }
 
